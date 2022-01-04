3b649ff3ffde96949d445865fe8d15b4d1c4cf84
Y2K222 <htetaungzaw811@gmail.com>
2022-01-04 10:33:07 +0630

    refactoring admin

--- routes/admins.js
+++ routes/admins.js
@@ -1,111 +1,90 @@
-const mongojs   = require('mongojs');
-const express   = require('express');
-const crypto    = require('crypto');
-
+const mongojs = require('mongojs');
+const express = require('express');
 const router = express.Router();
-const auth   = require('../auth');
-const db     = mongojs('movie_storage');
+const auth = require('../auth');
+const helper = require('../helper');
+
+// Mongodb
+const db = mongojs("movie_storage");
 
 // Get all admins and editors
 router.get('/', auth.ensureAdmin(), (req, res) => {
-    db.admins.find({}, (err, data) => {
-        if(err) res.status(500).json(err) // Internal server erroe
-        else if(!data) res.status(404)    // Not found
-        else res.status(200).json(data)   // Status OK
+    db.admins.find((err, data) => {
+        helper.respondToUser(res, err, data);
     });
 });
 
 // Get one admin
 router.get('/get_one/:id', auth.ensureAdmin(), (req, res) => {
-    let admin_id = req.params.id;
+    // Validation
     req.checkParams('id', 'Invalid user id').isMongoId();
     let validation_errors = req.validationErrors();
-    if(validation_errors) res.status(400).json(validation_errors)
-  
-    db.admins.findOne({_id: mongojs.ObjectId(admin_id)}, (err, data) => {
-        if(err) res.status(500).json(err);
-        else if(!data) res.status(404);
-        res.status(200).json(data);
+    if (validation_errors) res.status(400).json({ status: 400, message: 'Bad Request' })
+
+    db.admins.findOne({ _id: mongojs.ObjectId(req.params.id) }, (err, data) => {
+        helper.respondToUser(res, err, data);
     });
 });
 
 // Add new admin
 router.post('/', auth.ensureAdmin(), (req, res) => {
     // Validation
-    req.checkBody('full_name', 'Invalid Full Name').notEmpty();
-    req.checkBody('email', 'Invalid Email').isEmail();
-    req.checkBody('password', 'Invalid Password').isLength(6);
-    req.checkBody('role', 'Invalid Role').isInt();
-
+    req.checkBody('full_name', 'full_name should not be empty').notEmpty();
+    req.checkBody('email', 'email should not be empty').notEmpty()
+    req.checkBody('password', 'password should not be empty').notEmpty().isLength(6);
+    req.checkBody('role', 'role should be int').notEmpty().isInt();
     let validation_errors = req.validationErrors();
-    if(validation_errors) { res.status(400).json(validation_errors); return false; }
+    if (validation_errors) res.status(400).json({ status: 400, message: 'Bad Request' })
 
-    // Hash password
-    let hash_password = crypto.createHash('sha1').update(req.body.password).digest('hex');
+    // Modify request body
+    req.body.password = helper.hashPassword(req.body.password);
+    req.body.createdAt = new Date();
 
-    let new_admin = {
-        email     : req.body.email,
-        role      : req.body.role,
-        full_name : req.body.full_name,
-        password  : hash_password,
-        createdAt : new Date()
-    }
-    // Insert new user
-    db.admins.insert(new_admin, (err, data) => {
-        err ? res.status(500).json(err) : res.status(200).json({'msg': 'New admin created ', 'admin': data})
+    // Insert new Admin
+    db.admins.insert(req.body, (err, data) => {
+        helper.respondStatusToUser(res, err, data, { "msg": "New admin added" });
     });
 });
 
 // Update admin info
 router.put('/:id', auth.ensureAdmin(), (req, res) => {
     // Validation
-    req.checkParams('id', 'Invalid admin id').isMongoId();
-    req.checkBody('full_name', 'Invalid Full Name').notEmpty();
-    req.checkBody('email', 'Invalid Email').isEmail();
-
+    req.checkParams('id', 'admin id should be mongoId').notEmpty().isMongoId();
+    req.checkBody('full_name', 'full_name should not be empty').notEmpty();
+    req.checkBody('email', 'email should not be empty').notEmpty().isEmail();
     let validation_errors = req.validationErrors();
-    if(validation_errors) { res.status(400).json(validation_errors); return false; }
-
-    let updated_admin = {
-        full_name: req.body.full_name,
-        email    : req.body.email
-    }
+    if (validation_errors) res.status(400).json({ status: 400, message: 'Bad Request' })
 
     // Update
     db.admins.update(
-        {_id: mongojs.ObjectId(req.params.id)},
-        {$set: updated_admin},
-        {multi: false},
+        { _id: mongojs.ObjectId(req.params.id) },
+        { $set: req.body },
+        { multi: false },
         (err, data) => {
-            if(err) res.status(500).json(err);
-            else if(!data) res.status(404);
-            res.status(200).json({'msg': 'Admin updated', 'admin': data});
+            helper.respondStatusToUser(res, err, data, { "msg": "Admin data updated !" });
         }
     )
-
 });
 
-// Change password
+// Change admin password
 router.patch('/password/:id', auth.ensureAdmin(), (req, res) => {
+
     // Validation
-    req.checkParams('id', 'Invalid admin id').isMongoId();
-    req.checkBody('password', 'Invalid Password').notEmpty();
+    req.checkParams('id', 'Invalid admin id').notEmpty().isMongoId();
+    req.checkBody('password', 'password should not be empty').notEmpty();
     let validation_errors = req.validationErrors();
-    if(validation_errors) { res.status(400).json(validation_errors); return false; }
+    if (validation_errors) res.status(400).json({ status: 400, message: 'Bad Request' })
 
     // Hash password
-    let hash_password = crypto.createHash('sha1').update(req.body.password).digest('hex');
-    let updated_password = { password : hash_password };
+    req.body.password = helper.hashPassword(req.body.password);
 
     // Update the user
     db.admins.update(
-        {_id: mongojs.ObjectId(req.params.id)},
-        {$set: updated_password},
-        {multi: false},
+        { _id: mongojs.ObjectId(req.params.id) },
+        { $set: req.body },
+        { multi: false },
         (err, data) => {
-            if(err) res.status(500).json(err);
-            else if(!data) res.status(404);
-            res.status(200).json({'msg': 'Password updated', 'admin': data});
+            helper.respondStatusToUser(res, err, data, { "msg": "Admin password updated !" });
         }
     )
 });
@@ -113,47 +92,40 @@ router.patch('/password/:id', auth.ensureAdmin(), (req, res) => {
 // Change role
 router.patch('/role/:id', auth.ensureAdmin(), (req, res) => {
     // Validation
-    req.checkParams('id', 'Invalid admin id').isMongoId();
-    req.checkBody('role', 'Invalid Role').isInt();
+    req.checkParams('id', 'Invalid admin id').notEmpty().isMongoId();
+    req.checkBody('role', 'Invalid Role').notEmpty().isInt();
     let validation_errors = req.validationErrors();
-    if(validation_errors) { res.status(400).json(validation_errors); return false; }
+    if (validation_errors) res.status(400).json({ status: 400, message: 'Bad Request' })
 
-    // Update the user
-    let updated_role = { role: req.body.role };
+    // Modify the request body
     db.admins.update(
-        {_id: mongojs.ObjectId(req.params.id)},
-        {$set: updated_role},
-        {multi: false},
+        { _id: mongojs.ObjectId(req.params.id) },
+        { $set: req.body.role },
+        { multi: false },
         (err, data) => {
-            if(err) res.status(500).json(err);
-            else if(!data) res.status(404);
-            res.status(200).json({'msg': 'Role updated', 'admin': data});
+            helper.respondStatusToUser(res, err, data, { "msg": "Admin role updated !" });
         }
     )
-
 });
 
 // Delete admin
 router.delete('/:id', auth.ensureAdmin(), (req, res) => {
     // Validation
-    req.checkParams('id', 'Invalid admin id').isMongoId();
+    req.checkParams('id', 'admin id should be mongoId').isMongoId();
     let validation_errors = req.validationErrors();
-    if(validation_errors) { res.status(400).json(validation_errors); return false; }
+    if (validation_errors) res.status(400).json({ status: 400, message: 'Bad Request' })
 
     // Delete admin
     db.admins.remove(
-        {_id: mongojs.ObjectId(req.params.id)},
+        { _id: mongojs.ObjectId(req.params.id) },
         (err, data) => {
-            if(err) res.status(500).json(err);
-            else if(!data) res.status(404);
-            res.status(200).json({'msg': 'Admin deleted !'});
+            helper.respondStatusToUser(res, err, data, { "msg": "Admin deleted !" });
         }
     )
-
 });
 
-// verify that user is logged in
-router.get('/verify', auth.ensureAuth(), (req, res) => {
+// verify that admin is logged in
+router.get('/verify', auth.ensureAdmin(), (req, res) => {
     res.status(200).json(req.user);
 });
 
@@ -165,7 +137,7 @@ router.post('/login', auth.login(), (req, res) => {
 // Logout admin
 router.delete('/account/logout', (req, res) => {
     req.session.destroy();
-    res.status(200).json({'msg': 'Logged out'});
+    res.status(200).json({ 'msg': 'Logged out' });
 });
 
 module.exports = router;
